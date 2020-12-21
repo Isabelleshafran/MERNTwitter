2f0341b47d32aa17ecd72db1e5fcba72caa1804a
ishafran <isabelle.shafran@gmail.com>
2020-12-21 16:16:17 -0700

    add user register and login

--- models/User.js
+++ models/User.js
@@ -18,4 +18,4 @@ const UserSchema = new Schema({
   timestamps: true
 })
 
-module.export = User = mongoose.model('User', UserSchema)
+module.exports = User = mongoose.model('User', UserSchema)
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const keys = require('../../config/keys');
+const { validate } = require("../../models/User");
+
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ handle: req.body.handle }).then(user => {
+    if (user) {
+      errors.handle = "User already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              const payload = { id: user.id, handle: user.handle };
+
+              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password; 
+
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({email: 'This user does not exist'})
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        const payload = {id: user.id, handle: user.handle};
+
+                        jwt.sign(
+                            payload, 
+                            keys.secretOrKey, 
+                            {expiresIn: 3600}, 
+                            (err, token) => {
+                                res.json({
+                                    success: true, 
+                                    token: 'Bearer ' + token
+                                });
+                            });
+                    } else {
+                        return res.status(400).json({password: 'Incorrect Password'})
+                    }
+                })
+        })
+
+})
 
 module.exports = router;

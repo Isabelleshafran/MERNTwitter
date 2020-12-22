37aed751fb7394c1ba5011ff30ccf46e3d37b126
ishafran <isabelle.shafran@gmail.com>
2020-12-22 10:18:56 -0700

    tweet validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,18 +45,8 @@ router.post("/register", (req, res) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
           newUser.password = hash;
-          newUser
-            .save()
-            .then(user => {
-              const payload = { id: user.id, handle: user.handle };
-
-              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-                res.json({
-                  success: true,
-                  token: "Bearer " + token
-                });
-              });
-            })
+          newUser.save()
+            .then(user => res.json(user))
             .catch(err => console.log(err));
         });
       });
